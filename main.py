14fad2d23b954db133d9550345ee9319d354ab77
Jaime Santiago Barahona Moreno <santiagobarahona16@hotmail.com>
2025-10-19 20:48:35 -0500

    cambios para cargue

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hola desde Azure!"}
+# Archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Plantillas HTML
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
